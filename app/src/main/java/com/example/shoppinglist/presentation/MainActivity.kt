44d28e4079dd9380f847738588c12e80cc307490
EluAlty <145317718+EluAlty@users.noreply.github.com>
2024-05-01 18:10:16 +0500

    Domain layer without realisation

---
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.shoppinglist.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.shoppinglist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+
+
+
+
+        setContentView(binding.root)
+    }
+}
